92e28d6bc315f8f0b017f2cb94eaf6fe512bff7c
王成龙 <silence_ailong@qq.com>
2020-02-19 20:07:10 +0800

    修改I18本地化设置

--- lib/chapter9/Chapter9.dart
+++ lib/chapter9/Chapter9.dart
@@ -1,10 +1,19 @@
 import 'package:flutter/material.dart';
 import 'Chapter9Home.dart';
+import 'package:flutter_localizations/flutter_localizations.dart';
 
 class Chapter9 extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+        localizationsDelegates: [
+          GlobalMaterialLocalizations.delegate,
+          GlobalWidgetsLocalizations.delegate,
+        ],
+        supportedLocales: [
+          Locale('en', 'US'),
+          Locale('zh', 'CN'),
+        ],
         debugShowCheckedModeBanner: false, //显示开发模式标签
         initialRoute: '/',
         routes: {'/': (context) => Chapter9Home()},
@@ -12,6 +21,6 @@ class Chapter9 extends StatelessWidget {
             primarySwatch: Colors.blue,
             highlightColor: Color.fromRGBO(255, 255, 255, 0.5), //选中动画颜色
             splashColor: Colors.blue //水波纹
-        ));
+            ));
   }
 }
