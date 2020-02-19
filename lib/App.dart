fc79beb8c015272237c61b8079e5ce05c527ef64
二先生 <silence_ailong@qq.com>
2020-02-19 20:19:51 +0800

    修改本地化显示-->根(app)设置

--- lib/App.dart
+++ lib/App.dart
@@ -1,9 +1,19 @@
 import 'package:flutter/material.dart';
 import 'index.dart';
+import 'package:flutter_localizations/flutter_localizations.dart';
 class App extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+              localizationsDelegates: [
+          GlobalMaterialLocalizations.delegate,
+          GlobalWidgetsLocalizations.delegate,
+        ],
+        supportedLocales: [
+          Locale('en', 'US'),
+          Locale('zh', 'CN'),
+        ],
+        //locale: const Locale('zh'),
         debugShowCheckedModeBanner: false, //显示开发模式标签
         initialRoute: '/',
         routes: {
